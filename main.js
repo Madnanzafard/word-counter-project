98682e7c21f0c7dd88fe9c9bacbed055904d2a2d
Muhammad Adnan Zafar <160720380+Madnanzafard@users.noreply.github.com>
2024-04-17 13:25:35 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the words:",
+    }]);
+const word = answer.sentence.trim().split(" ");
+console.log(word);
+console.log(`Your sentence word count is ${word.length}`);
---
+++ main.ts
@@ -0,0 +1,13 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer: { sentence: string} = await inquirer.prompt([{
+    name : "sentence",
+    type : "input",
+    message : "Enter your sentence to count the words:",
+}])
+
+const word =answer.sentence.trim().split(" ");
+console.log (word);
+console.log(`Your sentence word count is ${word.length}`);
